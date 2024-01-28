09e9ae28221035adc934ea8c5f54b01300248598
egorduba <123907840+egorduba@users.noreply.github.com>
2024-01-29 00:27:29 +0200

    PZ2  Zadanie 1
    321312

---
+++ ConsoleApp1/MyClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MyClass
+    {
+        double value1, value2;
+        /// <summary>
+        /// целочисленное статическое поле для подсчета созданных экземпляров класса
+        /// </summary>
+        static int count;
+        /// <summary>
+        /// нестатический конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        public MyClass(double value1, double value2)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+            count++;
+        }
+        /// <summary>
+        ///  нестатический конструктор без параметров, который инициализирует внутренние поля 
+        /// </summary>
+        public MyClass()
+        {
+            this.Value1 = 2.0;
+            this.Value2 = 2.0;
+            count++;
+        }
+        /// <summary>
+        /// статический конструктор, который инициализирует статическую переменную нулевым значением
+        /// </summary>
+        static MyClass()
+        {
+            count = 0;
+        }
+        /// <summary>
+        /// свойства для доступа к внутренним полям класса Value1
+        /// </summary>
+        public double Value1
+        {
+            get
+            {
+                return value1;
+            }
+            set
+            {
+                value1 = value;
+            }
+        }
+        /// <summary>
+        /// свойства для доступа к внутренним полям класса Value2
+        /// </summary>
+        public double Value2
+        {
+            get
+            {
+                return value2;
+            }
+            set
+            {
+                value2 = value;
+            }
+        }
+        /// <summary>
+        /// метод для вывода на экран внутренних полей класса
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $" value1={Value1}\tvalue2={value2}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass1 = new MyClass();
+            MyClass myclass2 = new MyClass(20, 30);
+            Console.WriteLine(myclass2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
